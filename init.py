55c4e6ee350cf1b3c920c135a4ba44e62a69ab2e
Anthony Marchand <anthony.marchand@etu.unistra.fr>
2016-03-17 15:44:17 +0100

    Cleaned up the import section of init.py

--- init.py
+++ init.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from flask import Flask,request,render_template, jsonify
-app = Flask(__name__)
-
 from pymongo import MongoClient
+from json import dumps
+import crispr_tool
 
-import json, crispr_tool
 
-#import webbrowser
+
+app = Flask(__name__)
 
 contig_names_dict = {'A': 'NC_005967',
                      'B': 'NC_005968',
@@ -40,10 +40,10 @@ for annotation in db['annotations'].find():
 
 @app.route('/', methods=['GET', 'POST'])
 def init():
-    return render_template('index.html', data=json.dumps(gene_names))
+    return render_template('index.html', data=dumps(gene_names))
 
-@app.route('/casting_form')
-def castingform():
+@app.route('/crispr_form')
+def crisprform():
 
     result = None
     
@@ -70,24 +70,28 @@ def castingform():
         gc_max = request.args.get('gc_max', None, type=float)
 
     if region_type:
-        # print region_type
+
+        # Select by coordinates
         if region_type == 'collapse1':
             chromosome = request.args.get('chromosome', None, type=str)
             genomic_start = request.args.get('genomic_start', None, type=int)
             genomic_end = request.args.get('genomic_end', None, type=int)
             if chromosome and genomic_start and genomic_end:
                 result = crispr_tool.find(db_name='Candida_glabrata_CBS_138', genome_name=chromosome, start=genomic_start, end=genomic_end, pam_mode=pam_mode, search_mode=search_mode, gc_min=gc_min, gc_max=gc_max, mism=5, guides_file=None, host='localhost', port=27017, gene_name=None, sequence=None)
+
+        # Select by gene name
         elif region_type == 'collapse2':
             gene_name = request.args.get('gene_name', None, type=str)
             if gene_name:
                 result = crispr_tool.find(db_name='Candida_glabrata_CBS_138', genome_name=None, start=None, end=None, pam_mode=pam_mode, search_mode=search_mode, gc_min=gc_min, gc_max=gc_max, mism=5, guides_file=None, host='localhost', port=27017, gene_name=gene_name, sequence=None)
+        
+        # Select by sequence
         elif region_type == 'collapse3':
             sequence = request.args.get('sequence', None, type=str)
             if sequence:
                 result = crispr_tool.find(db_name='Candida_glabrata_CBS_138', genome_name=None, start=None, end=None, pam_mode=pam_mode, search_mode=search_mode, gc_min=gc_min, gc_max=gc_max, mism=5, guides_file=None, host='localhost', port=27017, gene_name=None, sequence=sequence)
-    # print "TEEEEEST", result
-    return json.dumps(result)
+
+    return dumps(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') #if app.run() => localhost by default
-    #webbrowser.open("http://127.0.0.1:5000")
